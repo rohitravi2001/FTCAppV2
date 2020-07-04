5222b3434b4f0d7bc7543513682b04a914acfaa3
Roohit Ravi <rohitravi2001@gmail.com>
2020-07-03 21:27:23 -0700

    Initial Commit

---
+++ FTCAppV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FTCAppV2
+//
+//  Created by Rohit Ravi on 7/3/20.
+//  Copyright © 2020 Rohit Ravi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
